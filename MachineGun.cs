f1788924b69f3f4bfb4b6a7608aefaa31583ce80
Eugene Volkov <bizzar@list.ru>
2021-03-10 23:08:51 +0300

    Add project files.

---
+++ MachineGun.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MachineGun_Console
+{
+    class MachineGun
+    {
+        //public const int MAGAZINE_SIZE = 16;
+        public int MagazineSize { get; private set; } = 16;
+
+        private int bullets = 0;
+        //private int bulletsLoaded = 0;
+
+        //public int GetBulletsLoaded() { return bulletsLoaded; }
+
+        //public int BulletsLoaded
+        //{ 
+        //    get { return bulletsLoaded; }
+        //    set { bulletsLoaded = value; }
+        //}
+
+        public int BulletsLoaded { get; private set; }
+
+        public MachineGun(int bullets, int magazineSize, bool loaded)
+        {
+            this.bullets = bullets;
+            MagazineSize = magazineSize;
+            if (!loaded) Reload();
+        }
+
+        public bool IsEmpty() { return BulletsLoaded == 0; }
+
+        // Create propery Bullets instead methods GetBullets, SetBullets
+        public int Bullets 
+        {
+            get { return bullets; }
+            set 
+            {
+                if (value > 0)
+                    bullets = value;
+                Reload();
+            }
+        }
+
+        //public int GetBullets() { return bullets; }
+
+        //public void SetBullets(int numberOfBullets)
+        //{
+        //    if (numberOfBullets > 0)
+        //        bullets = numberOfBullets;
+        //    Reload();
+        //}
+
+        public void Reload()
+        {
+            if (bullets > MagazineSize)
+                BulletsLoaded = MagazineSize;
+            else
+                BulletsLoaded = bullets;
+        }
+
+        public bool Shoot()
+        {
+            if (BulletsLoaded == 0) return false;
+            BulletsLoaded--;
+            bullets--;
+            return true;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace MachineGun_Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberOfBullets = ReadInt(20, "Number of bullets");
+            int magazineSize = ReadInt(16, "Magazine size");
+
+            Console.Write($"Loaded [false]: ");
+            bool.TryParse(Console.ReadLine(), out bool isLoaded);
+
+            MachineGun gun = new MachineGun(numberOfBullets, magazineSize, isLoaded);
+            while (true)
+            {
+                Console.WriteLine($"{gun.Bullets} bullets, {gun.BulletsLoaded} loaded");
+                if (gun.IsEmpty()) Console.WriteLine("WARNING: You're out of ammo");
+                
+                Console.WriteLine("Press space to shoot, r to reload, + to add ammo, q to quit");
+                char key = Console.ReadKey(true).KeyChar;
+                if (key == ' ') Console.WriteLine($"Shooting returned {gun.Shoot()}");
+                else if (key == 'r') gun.Reload();
+                else if (key == '+') gun.Bullets += gun.MagazineSize;
+                else if (key == 'q') return;
+            }
+        }
+
+        /// <summary>
+        /// Method from project AbilityScore
+        /// Writes a promt and reads an int value from the console
+        /// </summary>
+        /// <param name="lastUsedValue">The default value</param>
+        /// <param name="promt">Promt to print to the console</param>
+        /// <returns>The int value read, or the default value if unable to parse</returns>
+        private static int ReadInt(int lastUsedValue, string promt)
+        {
+            Console.WriteLine(promt + " [" + lastUsedValue + "]: ");
+            string line = Console.ReadLine();
+            if (int.TryParse(line, out int value))
+            {
+                Console.WriteLine("  using value " + value);
+                return value;
+            }
+            else
+            {
+                Console.WriteLine("  using default value " + lastUsedValue);
+                return lastUsedValue;
+            }
+        }
+    }
+}
